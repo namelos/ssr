8aaddbf23ffcb8a905ede672cebec9cafcd87445
Fred Wang <namelos@outlook.com>
2015-12-16 15:48:55 +0800

    recon modules

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from '../reducers'
+import rootReducer from '../modules'
 import createLogger from 'redux-logger'
 
 const logger = createLogger()
@@ -11,7 +11,7 @@ export const configureStore = initalState => {
   const store = createStoreWithMiddleware(rootReducer, initalState)
 
   if (module.hot) {
-    module.hot.accept('../reducers', () => {
+    module.hot.accept('../modules', () => {
       const nextRootReducer = require('../reducers')
       store.replaceReducer(nextRootReducer)
     })
