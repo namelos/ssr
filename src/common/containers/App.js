0f3a1f55f8d55f0c410e2b6be9b1539644868c5a
Fred Wang <namelos@outlook.com>
2015-12-16 12:07:36 +0800

    add app and components

---
+++ src/common/containers/App.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Counter from '../components/Counter'
+import * as CounterActions from '../actions/counter'
+
+const mapState = ({ counter }) => ({ counter })
+
+const mapDispatch = dispatch => bindActionCreators(CounterActions, dispatch)
+
+export default connect(mapState, mapDispatch())(Counter)
