f1095d57bc51605bec9fe68b5a9795a78622bdfd
Fred Wang <namelos@outlook.com>
2015-12-16 15:14:26 +0800

    finish minimal

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 import createLogger from 'redux-logger'
+
 const logger = createLogger()
 
 const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore)
