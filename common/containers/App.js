02ea6e180a952081b8508bfb055f8a46e541e864
Fred Wang <namelos@outlook.com>
2015-12-16 16:03:29 +0800

    switch to modules actions

--- common/containers/App.js
+++ common/containers/App.js
@@ -1,10 +1,11 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Counter from '../components/Counter'
-import * as CounterActions from '../actions/counter'
+import { increment, decrement, incrementIfOdd, incrementAsync } from '../modules/counter'
 
 const mapState = ({ counter }) => ({ counter })
 
-const mapDispatch = dispatch => bindActionCreators(CounterActions, dispatch)
+const mapDispatch = dispatch => bindActionCreators(
+  { increment, decrement, incrementIfOdd, incrementAsync }, dispatch)
 
 export default connect(mapState, mapDispatch)(Counter)
