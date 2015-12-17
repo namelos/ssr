b7926de7c9ce0a14bf7547a4d0c2d4734403dfe8
Fred Wang <namelos@outlook.com>
2015-12-17 12:15:09 +0800

    add presentation zone

--- containers/App.js
+++ containers/App.js
@@ -3,10 +3,11 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { TextField } from 'material-ui'
 import { Logo } from '../components/index'
-import { CompanyInfo, PersonalInfo, InstitutionInfo } from './index'
+import { CompanyInfo, PersonalInfo, InstitutionInfo, Presentation } from './index'
 
 const App = ({}) => <div>
   <CompanyInfo />
+  <Presentation />
   <PersonalInfo />
   <InstitutionInfo />
 </div>
---
+++ containers/Presentation.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Logo } from '../components/index'
+
+const Presentation = ({}) => <div>
+  <Logo width="1024" height="768" />
+</div>
+
+export default Presentation
